f4eaad2b26db8d940279f6b08d8633d5f856dcb7
xtream2000c <carlmarcast@gmail.com>
2022-01-14 20:54:12 +0100

    P2 imprime la primera semana

--- P2/JS/script.js
+++ P2/JS/script.js
@@ -7,6 +7,11 @@ function main(){
 }
 
 function imprimirCalendario(){
+
+    var hoy = new Date(); // Cojo el dia de hoy
+    var diaUno = new Date(); // Creo una fecha nueva
+    diaUno.setDate(1); // Digo que la fecha empieza en el dia 1
+    var diaSem = diaUno.getDay(); // Cojo la posicion del dia uno en la semana
     
     var contenedor = document.createElement('div');
     var tabla = document.createElement('table');
@@ -24,7 +29,24 @@ function imprimirCalendario(){
         fila.appendChild(cabeceraTabla);
     }
 
-    
+    fila = tabla.insertRow(1);
+
+    for (index = 0; index < diaSem-1; index++) {
+        var cabeceraTabla = document.createElement("td");
+        var texto = document.createTextNode(" ");
+        cabeceraTabla.appendChild(texto);
+        fila.appendChild(cabeceraTabla);
+    }
+    var diaSem1 = 1;
+    for (index = diaSem-1; index < 7; index++) {
+        var cabeceraTabla = document.createElement("td");
+        var texto = document.createTextNode(diaSem1);
+        cabeceraTabla.appendChild(texto);
+        fila.appendChild(cabeceraTabla);
+        diaSem1++;
+    }
+
+
 
     document.body.appendChild(contenedor);
 }
