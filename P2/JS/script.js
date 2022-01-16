e7ceb115a94716445659d3c350cc3696f1a7bad4
xtream2000c <carlmarcast@gmail.com>
2022-01-16 01:52:58 +0100

    Añadido switch para saber que mes es

--- P2/JS/script.js
+++ P2/JS/script.js
@@ -9,6 +9,8 @@ function main(){
 function imprimirCalendario(){
 
     var hoy = new Date(); // Cojo el dia de hoy
+    var mes = hoy.getMonth; // Cojo este mes
+    var year = hoy.getFullYear; //Cojo este año
     var diaUno = new Date(); // Creo una fecha nueva
     diaUno.setDate(1); // Digo que la fecha empieza en el dia 1
     var diaSem = diaUno.getDay(); // Cojo la posicion del dia uno en la semana
@@ -32,21 +34,58 @@ function imprimirCalendario(){
     fila = tabla.insertRow(1);
 
     for (index = 0; index < diaSem-1; index++) {
-        var cabeceraTabla = document.createElement("td");
+        var primeraSemana = document.createElement("td");
         var texto = document.createTextNode(" ");
-        cabeceraTabla.appendChild(texto);
-        fila.appendChild(cabeceraTabla);
+        primeraSemana.appendChild(texto);
+        fila.appendChild(primeraSemana);
     }
     var diaSem1 = 1;
     for (index = diaSem-1; index < 7; index++) {
-        var cabeceraTabla = document.createElement("td");
+        primeraSemana = document.createElement("td");
         var texto = document.createTextNode(diaSem1);
-        cabeceraTabla.appendChild(texto);
-        fila.appendChild(cabeceraTabla);
+        primeraSemana.appendChild(texto);
+        fila.appendChild(primeraSemana);
         diaSem1++;
     }
 
-
+    switch (mes){
+        case 0:
+            return 31;
+            break;
+        case 1:
+            return 28;
+            break;
+        case 2:
+            return 31;
+            break;
+        case 3:
+            return 30;
+            break;
+        case 4:
+            return 31;
+            break;
+        case 5:
+            return 30;
+            break;
+        case 6:
+            return 31;
+            break;
+        case 7:
+            return 31;
+            break;
+        case 8:
+            return 30;
+            break;
+        case 9:
+            return 31;
+            break;
+        case 10:
+            return 30;
+            break;
+        case 11:
+            return 31;
+            break;
+    }
 
     document.body.appendChild(contenedor);
 }
